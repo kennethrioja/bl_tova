2b7f4057dbbeee91fe23e8070603a200a724a125
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2023-05-08 22:45:28 +0200

    v.1.0.1 : modif strings

--- assets/js/bl_tova.js
+++ assets/js/bl_tova.js
@@ -49,7 +49,7 @@ var endblock_str2 = `
 <p>% of correctly refraining answers to BOTTOM square = `
 var endblock_str3 = `
 %</p>
-<p>Whenever you are ready, press the spacebar to continue the experiment.</p>
+<p>Press the spacebar to continue.</p>
 `;
 
 // #####################################
